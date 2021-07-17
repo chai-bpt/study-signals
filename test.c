0935fa20e27bad988323d188a608df4223f6174c
chai-bpt <chai.bpt@gmail.com>
2021-07-17 06:34:41 +0530

    signal handler chnaged run time to default and ignore, tested for SIGINT

--- test.c
+++ test.c
@@ -10,13 +10,25 @@ void signal_handler(int signal)
 
 int main(int argvc, char* argv[])
 {
-	printf("\n\tmy pid = %d",getpid());
+	typedef void (*sighandler_t)(int);
+	sighandler_t previous_handler = NULL;
 
-	signal(SIGINT, signal_handler);
+	printf("\n\tuser handler::signal_handler = %p\n",signal_handler);
+	
+	previous_handler = signal(SIGINT, signal_handler);
+	printf("\n\tprevious_handler = %p\n",previous_handler);
+	sleep(5);
 
-	while(1)
-	{
-		printf("\n\tlooping");
-		sleep(2);
-	}
+	previous_handler = signal(SIGINT, SIG_IGN);
+	printf("\n\tprevious_handler = %p\n",previous_handler);
+	sleep(5);
+
+        previous_handler = signal(SIGINT, SIG_DFL);
+        printf("\n\tprevious_handler = %p\n",previous_handler);
+        sleep(5);
+
+	printf("\n");
+
 }
+
+
