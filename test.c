62d6e4752c520a9c1e400ddb4e613ae9d4172004
chai-bpt <chai.bpt@gmail.com>
2021-07-17 07:39:48 +0530

    signal gets block during execution of handler and queued only once

--- test.c
+++ test.c
@@ -4,31 +4,20 @@
 
 void signal_handler(int signal)
 {
-	printf("\n\tsignal handler::signal = %d",signal);
+	printf("\n\tsignal handler::signal = %d\n",signal);
+	sleep(2);
+	printf("\n\tsignal handler::exit\n\t");
 	return;
 }
 
 int main(int argvc, char* argv[])
 {
-	typedef void (*sighandler_t)(int);
-	sighandler_t previous_handler = NULL;
-
-	printf("\n\tuser handler::signal_handler = %p\n",signal_handler);
 	
-	previous_handler = signal(SIGINT, signal_handler);
-	printf("\n\tprevious_handler = %p\n",previous_handler);
-	sleep(5);
-
-	previous_handler = signal(SIGINT, SIG_IGN);
-	printf("\n\tprevious_handler = %p\n",previous_handler);
-	sleep(5);
-
-        previous_handler = signal(SIGINT, SIG_DFL);
-        printf("\n\tprevious_handler = %p\n",previous_handler);
-        sleep(5);
+	signal(SIGINT, signal_handler);
+	printf("\n\tsignal handler registered\n");
+	sleep(10);
 
 	printf("\n");
-
 }
 
 
