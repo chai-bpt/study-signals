e8c4f99bbb2e7c4900b399cd3b66400d4bddb761
chai-bpt <chai.bpt@gmail.com>
2021-07-17 19:30:20 +0530

    set default handler once user register handler executed using SA_RESETHAND

--- test.c
+++ test.c
@@ -15,11 +15,15 @@ int main(int argvc, char* argv[])
 	struct sigaction act = {0};
 
 	act.sa_handler = signal_handler;
-	act.sa_flags = SA_NODEFER;
+	act.sa_flags = SA_RESETHAND;
 
 	sigaction(SIGINT, &act, NULL);
 
 	printf("\n\tsignal handler registered\n");
+
+	sleep(10);
+
+	printf("\n\tcheck default handler registered due to flag\n");
 
 	sleep(10);
 
