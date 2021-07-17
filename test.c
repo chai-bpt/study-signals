50ea5e0c809ad081cd1123f97f8113e348fe25f0
chai-bpt <chai.bpt@gmail.com>
2021-07-17 19:14:53 +0530

    signal handler regesteration check through sigaction

--- test.c
+++ test.c
@@ -12,9 +12,14 @@ void signal_handler(int signal)
 
 int main(int argvc, char* argv[])
 {
-	
-	signal(SIGINT, signal_handler);
+	struct sigaction act = {0};
+
+	act.sa_handler = signal_handler;
+
+	sigaction(SIGINT, &act, NULL);
+
 	printf("\n\tsignal handler registered\n");
+
 	sleep(10);
 
 	printf("\n");
