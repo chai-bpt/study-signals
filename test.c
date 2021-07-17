4a16b932debce01e5684063896455dfd2aa33901
chai-bpt <chai.bpt@gmail.com>
2021-07-17 05:56:55 +0530

    user defined signal handler tested for SIGINT

--- test.c
+++ test.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+
+void signal_handler(int signal)
+{
+	printf("\n\tsignal handler::signal = %d",signal);
+	return;
+}
 
 int main(int argvc, char* argv[])
 {
 	printf("\n\tmy pid = %d",getpid());
+
+	signal(SIGINT, signal_handler);
+
 	while(1)
 	{
 		printf("\n\tlooping");
