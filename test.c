3f2ac1f7f1e2006c70856593c726fbc369831870
chai-bpt <chai.bpt@gmail.com>
2021-07-18 12:28:40 +0530

    blocking and unblocking signals for a running process

--- test.c
+++ test.c
@@ -14,49 +14,31 @@ void signal_handler_sigint(int signal)
 	return;
 }
 
-void signal_handler_sigquit(int signal)
-{
-        printf("\n\tsignal handler SIGQUIT::signal = %d\n",signal);
-        sleep(1);
-        printf("\n\tsignal handler SIGQUIT::exit\n\t");
-        return;
-}
 int main(int argvc, char* argv[])
 {
-	struct sigaction act_int, act_quit, act_int_current;
-	sigset_t set_int, set_quit;
+	struct sigaction act_int;
+	sigset_t set;
 
 	memset(&act_int, 0, sizeof(act_int));
-	memset(&act_quit, 0, sizeof(act_quit));
-
-	sigemptyset(&set_int);
-	sigemptyset(&set_quit);
-
-	sigaddset(&set_int, SIGQUIT);
-
 	act_int.sa_handler = signal_handler_sigint;
-	act_int.sa_mask = set_int;
-
-	act_quit.sa_handler = signal_handler_sigquit;
-	
+	printf("\n\tRegistering SIGINT handler\n");
 	sigaction(SIGINT, &act_int, NULL);
-	sigaction(SIGQUIT,&act_quit, NULL);
-
-	printf("\n\tsignal handler registered\n");
-
+	printf("\n\tRegistered SIGINT handler\n");
 	sleep(10);
 
-	memset(&act_int_current, 0 , sizeof(act_int_current));
-	sigaction(SIGINT, NULL, &act_int_current);
+	sigemptyset(&set);
+        sigaddset(&set, SIGINT);
+	printf("\n\tBlocking SIGINT");
+        sigprocmask(SIG_BLOCK, &set, NULL);
+        printf("\n\tSIGINT blocked\n");
+	sleep(10);
 
-	set_int = act_int_current.sa_mask;
-
-	if(sigismember(&set_int, SIGQUIT))
-		printf("\n\tSIGQUIT is blocked\n");
-
-	if(sigismember(&set_int, SIGTERM))
-		printf("\n\tSIGTERM is blocked\n");
-
+	sigemptyset(&set);
+        sigaddset(&set, SIGINT);
+	printf("\n\tUnblocking SIGINT");
+        sigprocmask(SIG_UNBLOCK, &set, NULL);
+        printf("\n\tSIGINT unblocked\n");
+        sleep(10);
 
 	printf("\n");
 }
