c60e978ced5da4e0303c6bb7c3011b474233e0dc
chai-bpt <chai.bpt@gmail.com>
2021-07-29 11:49:30 +0530

    wait for a blocked signal within a process

--- test.c
+++ test.c
@@ -6,33 +6,41 @@
 #include <fcntl.h>
 #include <string.h>
 
-void signal_handler_sigint(int signal)
+void signal_handler(int signal)
 {
-	printf("\n\tsignal handler SIGINT::signal = %d\n",signal);
+	printf("\n\tsignal handler::signal = %d\n",signal);
 	sleep(1);
-	printf("\n\tsignal handler SIGINT::exit\n\t");
+	printf("\n\tsignal handler::exit\n\t");
 	return;
 }
 
 int main(int argvc, char* argv[])
 {
-	struct sigaction act_int;
+	struct sigaction act;
 	sigset_t set;
 	int i = 0;
+	siginfo_t info;
 
-	memset(&act_int, 0, sizeof(act_int));
-	act_int.sa_handler = signal_handler_sigint;
-	printf("\n\tRegistering SIGINT handler\n");
-	sigaction(SIGINT, &act_int, NULL);
-	printf("\n\tRegistered SIGINT handler\n");
-	sleep(10);
+	memset(&act, 0, sizeof(act));
+	memset(&info, 0, sizeof(info));
+
+	act.sa_handler = signal_handler;
+	printf("\n\tRegistering handler\n");
+	sigaction(SIGALRM, &act, NULL);
+	printf("\n\tRegistered handler\n");
+
+	alarm(3);
+	sleep(3);
 
 	sigemptyset(&set);
-        sigaddset(&set, SIGINT);
-	printf("\n\tBlocking SIGINT");
+        sigaddset(&set, SIGALRM);
+	printf("\n\tBlocking SIGALRM\n");
         sigprocmask(SIG_BLOCK, &set, NULL);
-        printf("\n\tSIGINT blocked\n");
+        printf("\n\tSIGALRM blocked\n");
 
+	alarm(3);
+	printf("\n\twating for signal\n");
+	sleep(3);
 	sigpending(&set);
 	for(i = 1; i < 65; i++)
 	{
@@ -42,27 +50,23 @@ int main(int argvc, char* argv[])
 		}
 	}
 
-
-	printf("\n\twating for signal\n");
-	sleep(10);
-
-
-	sigpending(&set);
-        for(i = 1; i < 65; i++)
-        {
-                if(sigismember(&set, i))
-                {
-                        printf("\n\t pending signal no = %d\n",i);
-                }
-        }
-
+        sigemptyset(&set);
+        sigaddset(&set, SIGALRM);
+	for(i = 0; i < 3; i++)
+	{ 
+		alarm(3);
+		sigwaitinfo(&set, &info);
+		printf("\n\treceived signal %d\n", info.si_signo);
+	}
 
 	sigemptyset(&set);
-        sigaddset(&set, SIGINT);
-	printf("\n\tUnblocking SIGINT");
+	sigaddset(&set, SIGALRM);
+	printf("\n\tUnblocking SIGALRM\n");
         sigprocmask(SIG_UNBLOCK, &set, NULL);
-        printf("\n\tSIGINT unblocked\n");
-        sleep(10);
+        printf("\n\tSIGALRM unblocked\n");
+
+	alarm(3);
+        sleep(3);
 
 	printf("\n");
 }
